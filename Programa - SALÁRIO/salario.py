933925d30502edea20365d0d22ce1738cfccb7f2
bruno jaen <86748458+brunojaen@users.noreply.github.com>
2022-03-25 12:47:23 -0300

    Add files via upload

--- Programa - SALÁRIO/salario.py
+++ Programa - SALÁRIO/salario.py
@@ -1,5 +1,5 @@
 salario_minimo = float(input("Informe seu salário: "))
-
 pretensao = float(input("Quantos salários você gostaria de ganhar: "))
+salario_desejado = salario_minimo * pretensao
 
-print("Você pretende ganhar R$", salario_minimo * pretensao)
+print("Você pretende ganhar R$", salario_desejado)
