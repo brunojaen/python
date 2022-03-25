f459fee17ea290cc80a5281375df60b61746dac1
bruno jaen <86748458+brunojaen@users.noreply.github.com>
2022-03-25 14:34:04 -0300

    Add files via upload

---
+++ Turtle/turtle.py
@@ -0,0 +1,12 @@
+import turtle
+
+colors = ["red", "purple", "blue", "green", "orange", "yellow"]
+
+pen = turtle.Pen()
+turtle.bgcolor("black")
+
+for x in range(360):
+    pen.pencolor(colors[x % 6])
+    pen.width(x/100 + 1)
+    pen.forward(x)
+    pen.left(59)
